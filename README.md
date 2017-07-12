a67fa00139509b09f4f80ad6f2bc50a02bdede77
Iyamoto <Iyamoto@users.noreply.github.com>
2017-07-13 00:10:16 +0300

    upload

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ git clone https://github.com/Iyamoto/pyweb.git
 ## to read
 * https://docs.docker.com/engine/reference/builder/
 * https://docs.docker.com/engine/userguide/eng-image/dockerfile_best-practices/#the-dockerfile-instructions
+* https://bottlepy.org/docs/dev/tutorial.html
--- rnd.py
+++ rnd.py
@@ -4,8 +4,12 @@ from bottle import route, run, template
 def index():
     return '<h1>pyweb</h1>'
 
-@route('/<action>/<user>')
-def user_api(action, user):
-    return template('<b>{{action}} {{user}}</b>!', action=action, user=user)
+@route('/health')
+def index():
+    return 'ok'
+
+@route('/v1/<action>/<param>')
+def user_api(action, param):
+    return template('<b>{{action}} {{param}}</b>!', action=action, user=param)
 
 run(host='0.0.0.0', port=8080)
