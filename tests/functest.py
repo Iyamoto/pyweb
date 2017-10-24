827aaa84546041efa0136444b9602e443cf6b064
iyamoto <iyamoto@gmail.com>
2017-10-25 00:18:13 +0300

    test status url

--- tests/functest.py
+++ tests/functest.py
@@ -7,19 +7,27 @@ import time
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         cmd = 'docker run -d --name pywebfunctest -p 8080:8080 iyamoto/pyweb:test'
-        subprocess.call(cmd, shell=True)
+        subprocess.call(cmd)
         self.url = 'http://127.0.0.1:8080/'
         time.sleep(2)
 
     def tearDown(self):
         cmd = 'docker rm -f pywebfunctest'
-        subprocess.call(cmd, shell=True)
+        subprocess.call(cmd)
 
     def test_ping_url(self):
-        pingurl = self.url + 'ping'
-        print(pingurl)
-        r = requests.get(pingurl)
+        url = self.url + 'ping'
+        print(url)
+        r = requests.get(url)
         print(r.status_code, r.reason, r.ok)
+        self.assertEqual(True, r.ok)
+
+    def test_status_url(self):
+        url = self.url + 'status'
+        print(url)
+        r = requests.get(url)
+        print(r.status_code, r.reason, r.ok)
+        print(r.json)
         self.assertEqual(True, r.ok)
 
 
