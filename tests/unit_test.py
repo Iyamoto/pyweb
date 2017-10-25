025d427f76bbf68366e9e713c0ddd1d78b15e540
iyamoto <iyamoto@gmail.com>
2017-10-25 22:48:40 +0300

    unit tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -3,8 +3,15 @@ import rnd
 
 
 class MyTestCase(unittest.TestCase):
+    def test_index(self):
+        assert rnd.index() == '<h1>pyweb</h1>'
+
     def test_ping(self):
         assert rnd.health() == 'ok'
+
+    def test_status(self):
+        assert type(rnd.status()) == dict
+        assert rnd.status() == {'bottle': 'UP'}
 
 
 if __name__ == '__main__':
