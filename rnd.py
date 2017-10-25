0633cfe91a57d572247184e2843ab05e0263c34e
iyamoto <iyamoto@gmail.com>
2017-10-25 22:24:23 +0300

    gevent disabled

--- rnd.py
+++ rnd.py
@@ -1,7 +1,7 @@
 # Researching Bottle
 
-from gevent import monkey
-monkey.patch_all()
+# from gevent import monkey
+# monkey.patch_all()
 from bottle import route, run, template
 
 
@@ -27,5 +27,5 @@ def status():
 def apiv1(action, param):
     return template('<b>{{action}} {{param}}</b>!', action=action, user=param)
 
-# run(server='gunicorn', host='0.0.0.0', port=8080, workers=4)
-run(host='0.0.0.0', port=8080, server='gevent')
+run(server='gunicorn', host='0.0.0.0', port=8080, workers=4)
+# run(host='0.0.0.0', port=8080, server='gevent')
