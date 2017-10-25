1a5ee5e9a20e468aa5294fc0f168553e99269aa5
iyamoto <iyamoto@gmail.com>
2017-10-25 22:53:26 +0300

    more fun

--- tests/stresstest.sh
+++ tests/stresstest.sh
@@ -2,5 +2,5 @@
 
 docker run -d --name pywebstresstest -p 8080:8080 iyamoto/pyweb:test
 sleep 2
-ab -n 5000 -v debug -c 10 http://127.0.0.1:8080/ping
+ab -n 10000 -v debug -c 100 http://127.0.0.1:8080/ping
 docker rm -f pywebstresstest
